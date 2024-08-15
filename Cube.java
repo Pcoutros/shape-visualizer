d12ab0fffe44364e23620b5bbe70f4c93764cf1a
Pcoutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-14 22:09:04 -0500

    Update Cube.java

--- Cube.java
+++ Cube.java
@@ -1,6 +1,5 @@
 /**
  * File Name: Cube.java
- * Course: CMSC335 7381
  * Date: 10/26/2023 
  * MODIFIED: 11/12/2023
  * Author: Pete Coutros
@@ -19,8 +18,6 @@
  * of an object of this class!
  * 
  * TYPE: REGULAR CUBE
- * 
- * @author Pete Coutros
  */
 import java.awt.Image;
 import javax.swing.ImageIcon;
