b5035046dd0a8fce550d039bec408227da514922
Pcoutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-14 22:08:09 -0500

    Update GUI.java
    Corrected Indentation Inconsistencies

--- GUI.java
+++ GUI.java
@@ -1,6 +1,5 @@
 /**
  * File Name: GUI.java
- * Course: CMSC335 7381
  * Date: 11/12/2023
  * Author: Pete Coutros
  * 
@@ -10,8 +9,6 @@
  * of a JSplitePane on the left and a JPanel on the right. The JSplitPane houses a JList on the left to select the shape and
  * houses JComboBox(es) on the right to select the parameter(s) of the shape. The JPanel on the right of the JFrame is used 
  * to house the corresponding shape drawings.
- * 
- * @author Pete Coutros
  */
 
 import java.awt.*;
@@ -23,63 +20,63 @@ public class GUI {
 	
 	//Attributes
 	private static final String[] OPTIONS = {"Circle", "Square", "Triangle", 
-			"Rectangle", "Sphere", "Cube", "Cone", "Cylinder", "Torus"};  		//Used to name selection options in JList				
+			"Rectangle", "Sphere", "Cube", "Cone", "Cylinder", "Torus"};  	//Used to name selection options in JList				
 	private static final Integer[] PARAMETER_DIMENSION = {null, 10, 50, 100};	//Used for parameter options in JComboBox
-	private JFrame frame;														//Main frame of GUI
-	private JList<String> list;													//List of shape options		
-	private JPanel parameterPanel;												//JPanel to house parameter JComboBox(es)
-	private JPanel drawingPanel;												//JPanel to house drawing displays of shapes, added to frame
-	private JSplitPane splitPane;												//SplitPane to house list and parameterPanel, added to frame
-	private JComboBox<Integer> firstParameterComboBox;							//JComboBox for drop down list of first parameter options
-	private JComboBox<Integer> secondParameterComboBox;							//JComboBox for drop down list of second parameter options
-	private int firstParameter, secondParameter;								//Used to store parameter selections
-	private TwoDimensionalShape twoDimensionalShape = null;						//Used polymorphically to create different TwoDimensionalShape objects
-	private ThreeDimensionalShape threeDimensionalShape = null;					//Used polymorphically to create different ThreeDimensionalShape objects
+	private JFrame frame;								//Main frame of GUI
+	private JList<String> list;							//List of shape options		
+	private JPanel parameterPanel;							//JPanel to house parameter JComboBox(es)
+	private JPanel drawingPanel;							//JPanel to house drawing displays of shapes, added to frame
+	private JSplitPane splitPane;							//SplitPane to house list and parameterPanel, added to frame
+	private JComboBox<Integer> firstParameterComboBox;				//JComboBox for drop down list of first parameter options
+	private JComboBox<Integer> secondParameterComboBox;				//JComboBox for drop down list of second parameter options
+	private int firstParameter, secondParameter;					//Used to store parameter selections
+	private TwoDimensionalShape twoDimensionalShape = null;				//Used polymorphically to create different TwoDimensionalShape objects
+	private ThreeDimensionalShape threeDimensionalShape = null;			//Used polymorphically to create different ThreeDimensionalShape objects
 	
 	/**
 	 * Constructor to create GUI using swing objects
 	 */
 	public GUI() {
-		frame = new JFrame("Project 2 - Shapes");								//Instantiate and initialize JFrame object
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);					//Close frame when exited
+		frame = new JFrame("Project 2 - Shapes");				//Instantiate and initialize JFrame object
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			//Close frame when exited
 		
-		list = new JList<String>(OPTIONS);										//Instantiate and initialize JList with shape options
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);				//Allow for only one selection from the list
+		list = new JList<String>(OPTIONS);					//Instantiate and initialize JList with shape options
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);		//Allow for only one selection from the list
 		
 		firstParameterComboBox = new JComboBox<Integer>(PARAMETER_DIMENSION);	//Instantiate and initialize JComboBox with first parameter dimension options
 		secondParameterComboBox = new JComboBox<Integer>(PARAMETER_DIMENSION);	//Instantiate and initialize JComboBox with second parameter dimension options
 
-		parameterPanel = new JPanel();											//Instantiate and initialize JPanel to house JComboBox(es)
-	    parameterPanel.setLayout(new GridLayout(0, 2));							//Set Layout so that it reads JLabel: JComboBox \n JLabel: JComboBox
-		parameterPanel.setPreferredSize(new Dimension(200, 400));				//Set Dimensions of the JPanel
+		parameterPanel = new JPanel();						//Instantiate and initialize JPanel to house JComboBox(es)
+	    	parameterPanel.setLayout(new GridLayout(0, 2));				//Set Layout so that it reads JLabel: JComboBox \n JLabel: JComboBox
+		parameterPanel.setPreferredSize(new Dimension(200, 400));		//Set Dimensions of the JPanel
 		
-		drawingPanel = new JPanel() {											//Instantiate and initialize JPanel to house the shape drawings
-            private static final long serialVersionUID = 1L;					//Unique Identifier
+		drawingPanel = new JPanel() {						//Instantiate and initialize JPanel to house the shape drawings
+            		private static final long serialVersionUID = 1L;				//Unique Identifier
 
 			@Override
-            /**
-             * Overrides the paintComponent() method of JComponent to draw a shape
-             * if a shape has been set. It will place the left top corner at the
-             * center of the screen.
-             * 
-             * @param Graphics g
-             */
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);											//Call paintComponent of JComponent
-                if (twoDimensionalShape != null) {									//If the shape is not null proceed
-                   twoDimensionalShape.draw(g, getWidth() / 2, getHeight() / 2); 	//Call draw method of Shape Class
-                }
-            }
+		        /**
+		     	* Overrides the paintComponent() method of JComponent to draw a shape
+		        * if a shape has been set. It will place the left top corner at the
+		        * center of the screen.
+		        * 
+		        * @param Graphics g
+		        */
+            		protected void paintComponent(Graphics g) {
+                		super.paintComponent(g);						//Call paintComponent of JComponent
+                		if (twoDimensionalShape != null) {					//If the shape is not null proceed
+                   			twoDimensionalShape.draw(g, getWidth() / 2, getHeight() / 2); 	//Call draw method of Shape Class
+                		}
+            		}
 		};
-		drawingPanel.setPreferredSize(new Dimension(400, 400));						//Set Dimensions of the JPanel
+		drawingPanel.setPreferredSize(new Dimension(400, 400));				//Set Dimensions of the JPanel
 		
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, list, parameterPanel);	//Add list and parameterPanel to JSplitPane
 		
-		frame.add(splitPane, BorderLayout.WEST);									//Add splitPane on left hand side of frame
-		frame.add(drawingPanel, BorderLayout.EAST);									//Add drawingPanel on right hand side of frame
+		frame.add(splitPane, BorderLayout.WEST);					//Add splitPane on left hand side of frame
+		frame.add(drawingPanel, BorderLayout.EAST);					//Add drawingPanel on right hand side of frame
 		
-		addFunctionalityToList();													//Give functionality to JList
-		addFunctionalityToComboBox();												//Give functionality to JComboBox
+		addFunctionalityToList();							//Give functionality to JList
+		addFunctionalityToComboBox();							//Give functionality to JComboBox
 	}
 	
 	/**
@@ -88,10 +85,10 @@ public class GUI {
 	 * the parameterPanel by calling the updateParameterPanel() method.
 	 */
 	private void addFunctionalityToList() {
-        list.addListSelectionListener((ListSelectionEvent e) -> {
-        	setShapesToNull();													//Set shapes to Null to reset any prior drawings
-            updateParameterPanel();												//Update the parameterPanel to display JComboBoxes
-        });
+        	list.addListSelectionListener((ListSelectionEvent e) -> {
+        		setShapesToNull();							//Set shapes to Null to reset any prior drawings
+            		updateParameterPanel();							//Update the parameterPanel to display JComboBoxes
+        	});
 	}
 	
 	/**
@@ -107,12 +104,12 @@ public class GUI {
 			try {
 				firstParameter = (int) firstParameterComboBox.getSelectedItem();		//Store first parameter selection as int
 				if (secondParameterComboBox.isVisible() &&
-					secondParameterComboBox.getSelectedItem() == null) {				//Check if parameterPanel has a second JComboBox and if it is null					
-					secondParameter = 0;												//Set secondParameter to 0
+					secondParameterComboBox.getSelectedItem() == null) {			//Check if parameterPanel has a second JComboBox and if it is null					
+					secondParameter = 0;							//Set secondParameter to 0
 				}
-		        processSelection();														//Call to process the parameter selection
-			} catch (NullPointerException npe) {										//Catch a null entry from the JComboBox
-				resetDrawingPanel();													//Clear the drawingPanel and inform user to select a parameter value
+		        processSelection();									//Call to process the parameter selection
+			} catch (NullPointerException npe) {							//Catch a null entry from the JComboBox
+				resetDrawingPanel();								//Clear the drawingPanel and inform user to select a parameter value
 			}
 		});
 		
@@ -121,12 +118,12 @@ public class GUI {
 				firstParameter = (int) firstParameterComboBox.getSelectedItem();		//Store first parameter selection as int
 				secondParameter = (int) secondParameterComboBox.getSelectedItem();		//Store second parameter selection as int
 					if (firstParameter != 0 && secondParameter != 0) {
-				        processSelection();												//Call to procesSelection if both parameters are not null
+				        processSelection();							//Call to procesSelection if both parameters are not null
 					} else {
-						throw new NullPointerException();								//If the parameters are 0 (default value), no selection has been made.
+						throw new NullPointerException();				//If the parameters are 0 (default value), no selection has been made.
 					}
-			} catch (NullPointerException npe) {										//Catch a null entry from the JComboBox
-				resetDrawingPanel();													//Clear the drawingPanel and inform user to select a parameter value
+			} catch (NullPointerException npe) {							//Catch a null entry from the JComboBox
+				resetDrawingPanel();								//Clear the drawingPanel and inform user to select a parameter value
 			}
 		});
 	}
@@ -175,9 +172,9 @@ public class GUI {
 	 * the user.
 	 */
 	private void processSelection() {
-		drawingPanel.removeAll();												//Remove prior drawing/images
+		drawingPanel.removeAll();							//Remove prior drawing/images
 		if (getNumberOfParameters(firstParameter, secondParameter) == 1) {		//If only the first parameter is selected, process shapes with one parameter
-			switch(list.getSelectedValue()) {									//Base flow off of user selection from JList to create corresponding shape
+			switch(list.getSelectedValue()) {					//Base flow off of user selection from JList to create corresponding shape
 			case "Circle":							
 				twoDimensionalShape = new Circle(firstParameter);				
 				break;	
@@ -192,8 +189,8 @@ public class GUI {
 			break;
 			}
 		}	
-		if (getNumberOfParameters(firstParameter, secondParameter) == 2) {				//If both parameters are selected, process shapes with two parameters
-			switch(list.getSelectedValue()) {											//Base flow off of user selection from JList to create corresponding shape
+		if (getNumberOfParameters(firstParameter, secondParameter) == 2) {		//If both parameters are selected, process shapes with two parameters
+			switch(list.getSelectedValue()) {					//Base flow off of user selection from JList to create corresponding shape
 			case "Triangle":
 				twoDimensionalShape = new Triangle(firstParameter, secondParameter);	
 				break;
@@ -207,14 +204,14 @@ public class GUI {
 				threeDimensionalShape = new Cylinder(firstParameter, secondParameter);
 				break;
 			case "Torus":
-				if (firstParameter > secondParameter) {										//Radius Major must be larger than Radius Minor
+				if (firstParameter > secondParameter) {						//Radius Major must be larger than Radius Minor
 					threeDimensionalShape = new Torus(firstParameter, secondParameter);		
 					break;
 				} else {																	//If Radius Major is less than or equal to Radius Minor it is invalid
 					resetDrawingPanel();													//Clear the drawingPanel and inform user to select a parameter value
 					JOptionPane.showMessageDialog(drawingPanel, 
 							new JLabel("Radius Major needs to be greater than Radius Minor!"),
-							null, JOptionPane.ERROR_MESSAGE);								//Display error message wrapped in JLabel to a JOptionPane pop up
+							null, JOptionPane.ERROR_MESSAGE);					//Display error message wrapped in JLabel to a JOptionPane pop up
 				}
 			break;
 			}
@@ -223,12 +220,12 @@ public class GUI {
 			drawingPanel.repaint();									//Repaint the drawingPanel by indirectly calling paintComponent() to display the 2D shape
 		}
 		if (threeDimensionalShape != null) {
-			display3DImage(threeDimensionalShape);										//Call to display the generic image of the 3D shape
-			if (list.getSelectedValue().equals("Torus")) {								//Display FYI message if shape is a torus
+			display3DImage(threeDimensionalShape);							//Call to display the generic image of the 3D shape
+			if (list.getSelectedValue().equals("Torus")) {						//Display FYI message if shape is a torus
 				JOptionPane.showMessageDialog(drawingPanel, 								
 						new JLabel("Note: Torus size will not change with varying "
 								+ "Radii sizes due to its complex shape."), 
-								"FYI", JOptionPane.INFORMATION_MESSAGE);				//Note to the user to not expect a change in size for the Torus
+								"FYI", JOptionPane.INFORMATION_MESSAGE);	//Note to the user to not expect a change in size for the Torus
 			}
 		}
 	}
@@ -243,15 +240,15 @@ public class GUI {
 	 */
 	private void display3DImage(ThreeDimensionalShape shape) {
 		drawingPanel.removeAll();																	//Clear any prior labels or drawings
-	    JLabel image = new JLabel(shape.getImage(firstParameter, secondParameter)); 				//Get image from shape and wrap in JLabel
-
-	    FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);									//Used to center image. Needs to be stored to set H/Vgaps
-	    flowLayout.setHgap((drawingPanel.getWidth() - image.getIcon().getIconWidth()) / 2);			//Set horizontal gap to center horizontally
-	    flowLayout.setVgap((drawingPanel.getHeight() - image.getIcon().getIconHeight()) / 2);		//Set vertical gap to center vertically
-
-	    drawingPanel.setLayout(flowLayout);															//Set layout of drawingPanel to the new FlowLayout
-	    drawingPanel.add(image);																	//Add the image to the panel with new layout
-	    drawingPanel.updateUI();																	//Update the panel to show the image
+		JLabel image = new JLabel(shape.getImage(firstParameter, secondParameter)); 			//Get image from shape and wrap in JLabel
+	
+		FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);					//Used to center image. Needs to be stored to set H/Vgaps
+	    	flowLayout.setHgap((drawingPanel.getWidth() - image.getIcon().getIconWidth()) / 2);		//Set horizontal gap to center horizontally
+		flowLayout.setVgap((drawingPanel.getHeight() - image.getIcon().getIconHeight()) / 2);		//Set vertical gap to center vertically
+	
+		drawingPanel.setLayout(flowLayout);															//Set layout of drawingPanel to the new FlowLayout
+		drawingPanel.add(image);																	//Add the image to the panel with new layout
+		drawingPanel.updateUI();																	//Update the panel to show the image
 	}
 	
 	/**
@@ -260,15 +257,15 @@ public class GUI {
 	 * value when no selection from the JComboBox is made.
 	 */
 	private void resetParameterPanel() {
-		parameterPanel.removeAll();							//Remove previous JLabel(s)/JComboBox(es) from parameterPanel
+		parameterPanel.removeAll();				//Remove previous JLabel(s)/JComboBox(es) from parameterPanel
 		
-		firstParameterComboBox.setSelectedIndex(0);			//Reset the JComboBox to the first option
+		firstParameterComboBox.setSelectedIndex(0);		//Reset the JComboBox to the first option
 		secondParameterComboBox.setSelectedIndex(0);		//Reset the JComboBox to the first option
 		
-		parameterPanel.updateUI();							//Update the parameterPanel
+		parameterPanel.updateUI();				//Update the parameterPanel
 		
-		firstParameter = 0;									//Reset value of the first parameter to 0, which is the default value when no selection is made
-		secondParameter = 0;								//Reset value of the second parameter to 0, which is the default value when no selection is made
+		firstParameter = 0;					//Reset value of the first parameter to 0, which is the default value when no selection is made
+		secondParameter = 0;					//Reset value of the second parameter to 0, which is the default value when no selection is made
 	}
 	
 	/**
@@ -278,8 +275,8 @@ public class GUI {
 	 * 
 	 */
 	private void updateParameterPanel() {
-		resetParameterPanel();								//Reset the parameterPanel
-		switch(list.getSelectedValue()) {					//Base flow off of user selection from JList
+		resetParameterPanel();					//Reset the parameterPanel
+		switch(list.getSelectedValue()) {			//Base flow off of user selection from JList
 		case "Circle":
 			addLabelAndComboBox("Radius:", null);
 			break;
@@ -308,7 +305,7 @@ public class GUI {
 			addLabelAndComboBox("Radius Major:", "Radius Minor:");
 			break;
 		}
-		parameterPanel.updateUI();							//Update the changes to the parameterPanel
+		parameterPanel.updateUI();				//Update the changes to the parameterPanel
 	}
 	
 	/**
@@ -319,7 +316,7 @@ public class GUI {
 	 */
 	private void addLabelAndComboBox(String firstDescription, String secondDescription) {
 		parameterPanel.add(new JLabel(firstDescription));				//Add label for the parameter
-		parameterPanel.add(firstParameterComboBox);						//Add JComboBox to select parameter value
+		parameterPanel.add(firstParameterComboBox);					//Add JComboBox to select parameter value
 		if (secondDescription != null) {
 			parameterPanel.add(new JLabel(secondDescription));			//Add label for second parameter
 			parameterPanel.add(secondParameterComboBox);				//Add JComboBox to select second parameter value
